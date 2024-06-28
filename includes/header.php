dc217ffcc2411b03a9b6c0a1633e88ff565e9607
Tabish221 <143192083+Tabish221@users.noreply.github.com>
2024-06-28 19:09:34 +0500

    op

---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<header>
+	<div class="main-header">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-md-2">
+					<div class="side-menuBar">
+						<div class="menu-Bar">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+						<span>MORE</span>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="menuWrap">
+						<ul class="menu">
+							<li><a class="active" href="./">HOME</a></li>
+							<li><a href="#">ABOUT SGL</a></li>
+							<li><a href="#">EMPOWERING MINERS</a></li>
+						</ul>
+
+						<a href="./" class="logo">
+							<img src="assets/images/logo1.png" alt="">
+						</a>
+
+						<ul class="menu">
+							<li><a class="active" href="./">SGL REFINERY</a></li>
+							<li><a href="#">NEWSROOM</a></li>
+							<li><a href="#">OUR TEAM</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div class="header-action">
+						<div class="header-search">
+							<i class="far fa-search"></i>
+						</div>
+
+						<div class="header-lang">
+							<span>EN</span>
+						</div>
+
+						<div class="header-btn">
+							<a href="#">CONTACT US</a>
+						</div>
+					</div>
+				</div>   
+			</div>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <?php include("includes/compatibility.php"); ?>
+      <meta name="description" content="">
+      <title>Title Here</title>
+      <?php include("includes/style.php"); ?>
+   </head>
+   <body>
+      <?php include("includes/header.php"); ?>
+
+      <div class="mainBanner" style="background-image: url(assets/images/banner/bg-banner.jpg);">
+         <div class="container">
+            <div class="mainBanner-cont">
+               <h2>Unrivaled Excellence in</h2>
+               <h1>Gold Refining</h1>
+               <p>SGL is a Tanzanian-based gold refinery, leveraging decade-long expertise to refine and trade fine gold bullion while prioritising the well-being of miners and the environment.</p>
+               <p>At SGL, it's more than gold; it's about people.</p>
+
+               <div class="mn-btn">
+                  <a href="#">WATCH THE FILM</a>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <?php include("includes/footer.php"); ?>
+      <?php include("includes/scripts.php"); ?>
+   </body>
+</html>
