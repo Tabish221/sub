7f15aa4131fd6bad9db4e5636d5608513c2a6cfe
Tabish221 <tabishtanveer46@gmail.com>
2024-07-01 10:18:03 +0500

    new

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,75 @@
 <footer>
-   <div class="container">
-   	<div class="row">
-   		<div class="col-md-4">
-   			<div class="widget">
-   				
-   			</div>
-   		</div>
-   		<div class="col-md-4">
-   			<div class="widget">
-   				
-   			</div>
-   		</div>
-   		<div class="col-md-4">
-   			<div class="widget">
-   				
-   			</div>
-   		</div>
-   	</div>
-     
-   </div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="footerBackBtn">
+					<a href="#">
+						<div class="icon">
+							<svg width="14" height="8" viewBox="0 0 14 8" fill="none"
+								xmlns="http://www.w3.org/2000/svg">
+								<path d="M1.75 6.875L7 1.625L12.25 6.875" stroke="white" stroke-width="2"
+									stroke-linecap="round" stroke-linejoin="round" />
+							</svg>
+						</div>
+						Back to top
+					</a>
+				</div>
+
+				<div class="footerLogo">
+					<img src="assets/images/footer-logo.png" alt="Logo">
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="widget">
+					<div class="getTouch-footerForm">
+						<label for="">Get updates on the latest industry trends.</label>
+						<div class="getTouch-footerFormFeild">
+							<input type="text" placeholder="Email Address">
+							<button>OK</button>
+						</div>
+					</div>
+
+					<div class="footerLink">
+						<ul>
+							<li><a href="#">Industries</a></li>
+							<li><a href="#">Surfaces</a></li>
+							<li><a href="#">Catalog</a></li>
+						</ul>
+					</div>
+
+					<div class="footerLink">
+						<ul>
+							<li><a href="#">Technologies</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">About</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
 
-<div class="copyright text-center">
- 	<div class="container">
-      <p>All Rights Reserved</p>
-   </div>
+<div class="copyright">
+	<div class="container-fluid">
+		<div class="row align-items-end">
+			<div class="col-md-6">
+				<p>All Rights Reserved</p>
+			</div>
+
+			<div class="col-md-6 text-end">
+				<ul>
+					<li>
+						<span>Email</span>
+						<a href="mailto:info@sabgoldlimited.com">info@sabgoldlimited.com</a>
+					</li>
+
+					<li>
+						<span>Phone</span>
+						<a href="tel:+41 123 3456 789">+41 123 3456 789</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
